b13b7bc9678d04c0a7727c7b26be1e060af97847
kevinwanyama <56598203+kevinwanyama@users.noreply.github.com>
2021-08-31 12:59:01 +0300

    fragments

--- app/src/main/java/com/example/hsmicrofinance/Fragments/BasicSettings.java
+++ app/src/main/java/com/example/hsmicrofinance/Fragments/BasicSettings.java
@@ -2,6 +2,8 @@ package com.example.hsmicrofinance.Fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -28,5 +30,11 @@ public class BasicSettings extends Fragment {
         // Inflate the layout for this fragment
         mFragmentBasicSettingsBinding = FragmentBasicSettingsBinding.inflate(inflater,container,false);
         return mFragmentBasicSettingsBinding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mFragmentBasicSettingsBinding.submitbutton.setOnClickListener();
     }
 }
--- app/src/main/java/com/example/hsmicrofinance/Fragments/PasswordChangeFragment.java
+++ app/src/main/java/com/example/hsmicrofinance/Fragments/PasswordChangeFragment.java
@@ -1,4 +1,4 @@
-package com.example.hsmicrofinance;
+package com.example.hsmicrofinance.Fragments;
 
 import android.os.Bundle;
 
@@ -8,8 +8,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.hsmicrofinance.R;
 
-public class BillRequestFragment extends Fragment {
+
+public class PasswordChangeFragment extends Fragment {
 
 
 
@@ -17,6 +19,6 @@ public class BillRequestFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_bill_request, container, false);
+        return inflater.inflate(R.layout.fragment_password_change, container, false);
     }
 }
